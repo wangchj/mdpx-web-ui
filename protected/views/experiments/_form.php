afb9ac451f67bcec0ed7ddfe52530da2b7d53aeb
Jeff Wang <cjw39@hotmail.com>
2013-07-30 22:55:30 -0500

    Change to new layout.

--- protected/views/experiments/_form.php
+++ protected/views/experiments/_form.php
@@ -15,43 +15,43 @@
 
 	<?php echo $form->errorSummary($model); ?>
 
-	<div class="row">
+	<div>
 		<?php echo $form->labelEx($model,'experimentId'); ?>
 		<?php echo $form->textField($model,'experimentId'); ?>
 		<?php echo $form->error($model,'experimentId'); ?>
 	</div>
 
-	<div class="row">
+	<div>
 		<?php echo $form->labelEx($model,'name'); ?>
 		<?php echo $form->textField($model,'name',array('size'=>45,'maxlength'=>45)); ?>
 		<?php echo $form->error($model,'name'); ?>
 	</div>
 
-	<div class="row">
+	<div>
 		<?php echo $form->labelEx($model,'description'); ?>
 		<?php echo $form->textField($model,'description',array('size'=>45,'maxlength'=>45)); ?>
 		<?php echo $form->error($model,'description'); ?>
 	</div>
 
-	<!-- div class="row">
+	<?php /* <div>
 		<?php echo $form->labelEx($model,'dateTime'); ?>
 		<?php echo $form->textField($model,'dateTime'); ?>
 		<?php echo $form->error($model,'dateTime'); ?>
-	</div -->
+	</div> */?>
 
-	<div class="row">
+	<div>
 		<?php echo $form->labelEx($model,'researcherId'); ?>
 		<?php echo $form->dropDownList($model, 'researcherId', Users::getDropdownList()) ?>
 		<?php echo $form->error($model,'researcherId'); ?>
 	</div>
 
-	<div class="row">
+	<div>
 		<?php echo $form->labelEx($model,'operatorId'); ?>
         <?php echo $form->dropDownList($model, 'operatorId', Users::getDropdownList()) ?>
 		<?php echo $form->error($model,'operatorId'); ?>
 	</div>
 
-	<div class="row buttons">
+	<div class="buttons">
 		<?php echo CHtml::submitButton($model->isNewRecord ? 'Create' : 'Save'); ?>
         <?php echo CHtml::button('Cancel', array('id'=>'cancelBtn')); ?>
 	</div>

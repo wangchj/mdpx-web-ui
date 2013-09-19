29f550aa964b6850ee3ce1a6777f7c0f0b61ea97
Jeff Wang <cjw39@hotmail.com>
2013-09-18 21:52:23 -0500

    Add hasAccess() and hasAnyAccess()

--- protected/components/Controller.php
+++ protected/components/Controller.php
@@ -20,4 +20,42 @@ class Controller extends CController
 	 * for more details on how to specify this property.
 	 */
 	public $breadcrumbs=array();
+
+    /**
+     * Checks if the current user has access to the operation defined by controller and action.
+     * @param $controllerId
+     * @param $actionId
+     * @return boolean true if the current user has access to the operation;
+     * false otherwise
+     */
+    public function hasAccess($controllerId, $actionId)
+    {
+        $user = Yii::app()->user;
+        $am = Yii::app()->accessManager;
+
+        return $am->hasAccess($user->id, $controllerId, $actionId);
+    }
+
+    public function hasAnyAccess($routes=array())
+    {
+        if(count($routes) == 0) return false;
+
+        foreach($routes as $route)
+        {
+            //No controller ID specified. Use current controller ID.
+            if(!strpos($route, '/'))
+            {
+                if($this->hasAccess($this->id, $route))
+                    return true;
+            }
+            else
+            {
+                $r = explode('/', $route);
+                if($this->hasAccess($r[0], $r[1]))
+                    return true;
+            }
+        }
+
+        return false;
+    }
 }

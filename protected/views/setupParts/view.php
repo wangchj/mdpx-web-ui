aa203067de316e45bd09bd7c9b910fe8db7ebabd
Jeff Wang <cjw39@hotmail.com>
2013-09-26 16:09:29 -0500

    Show additional information for cameras and probes.

--- protected/views/setupParts/view.php
+++ protected/views/setupParts/view.php
@@ -5,13 +5,49 @@
 
 <h1>View Setup Part #<?php echo $model->setupPartId; ?></h1>
 
-<?php $this->widget('zii.widgets.CDetailView', array(
-	'data'=>$model,
-	'attributes'=>array(
-		array('label'=>'Setup Part Seq', 'name'=>'setupPartId'),
-        array('label'=>'Vessel Setup', 'value'=>$model->vesselSetup->name . ' (#' . $model->vesselSetupId . ')'),
-		array('label'=>'Part Name', 'value'=>$model->part0->type0->name . ' (' . $model->part0->serialNum . ')'),
-        array('label'=>'Relative Location', 'name'=>'port'),
-		'parent',
-	),
-)); ?>
+<?php
+if($model->isCamera())
+{
+    $this->widget('zii.widgets.CDetailView', array(
+        'data'=>$model,
+        'attributes'=>array(
+            array('label'=>'Setup Part Seq', 'name'=>'setupPartId'),
+            array('label'=>'Vessel Setup', 'value'=>$model->vesselSetup->name . ' (#' . $model->vesselSetupId . ')'),
+            array('label'=>'Part Name', 'value'=>$model->part0->type0->name . ' (' . $model->part0->serialNum . ')'),
+            array('label'=>'Position R', 'value'=>$model->setupCameras != null ? $model->setupCameras->positionR : null),
+            array('label'=>'Position Z', 'value'=>$model->setupCameras != null ? $model->setupCameras->positionZ : null),
+            array('label'=>'Lens', 'value'=>$model->setupCameras != null ? $model->setupCameras->lens0->type0->name . ' (' . $model->setupCameras->lens . ')' : null),
+            array('label'=>'Filter', 'value'=>$model->setupCameras != null ? $model->setupCameras->filter0->type0->name . ' (' . $model->setupCameras->filter . ')' : null),
+            array('label'=>'Relative Location', 'name'=>'port'),
+            'parent',
+        ),
+    ));
+}
+elseif($model->isProbe())
+{
+    $this->widget('zii.widgets.CDetailView', array(
+        'data'=>$model,
+        'attributes'=>array(
+            array('label'=>'Setup Part Seq', 'name'=>'setupPartId'),
+            array('label'=>'Vessel Setup', 'value'=>$model->vesselSetup->name . ' (#' . $model->vesselSetupId . ')'),
+            array('label'=>'Part Name', 'value'=>$model->part0->type0->name . ' (' . $model->part0->serialNum . ')'),
+            array('label'=>'Length', 'value'=>$model->setupProbes != null ? $model->setupProbes->length : null),
+            array('label'=>'Width', 'value'=>$model->setupProbes != null ? $model->setupProbes->width : null),
+            array('label'=>'Relative Location', 'name'=>'port'),
+            'parent',
+        ),
+    ));
+}
+else{
+    $this->widget('zii.widgets.CDetailView', array(
+        'data'=>$model,
+        'attributes'=>array(
+            array('label'=>'Setup Part Seq', 'name'=>'setupPartId'),
+            array('label'=>'Vessel Setup', 'value'=>$model->vesselSetup->name . ' (#' . $model->vesselSetupId . ')'),
+            array('label'=>'Part Name', 'value'=>$model->part0->type0->name . ' (' . $model->part0->serialNum . ')'),
+            array('label'=>'Relative Location', 'name'=>'port'),
+            'parent',
+        ),
+    ));
+}
+?>

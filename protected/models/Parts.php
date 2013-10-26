2fc07c100345e3ef058adb8d17d34f22608357c9
Jeff Wang <cjw39@hotmail.com>
2013-10-26 15:33:37 -0500

    Sync with schema version 9.

--- protected/models/Parts.php
+++ protected/models/Parts.php
@@ -5,7 +5,7 @@
  *
  * The followings are the available columns in table 'Parts':
  * @property string $serialNum
- * @property integer $type
+ * @property string $type
  * @property string $addedOn
  * @property integer $addedBy
  *
@@ -14,13 +14,7 @@
  * @property Users $addedBy0
  * @property SetupCameras[] $setupCamerases
  * @property SetupCameras[] $setupCamerases1
- * @property SetupCameras[] $setupCamerases2
- * @property VesselPlates[] $vesselPlates
- * @property VesselSetup[] $vesselSetups
- * @property VesselSetup[] $vesselSetups1
- * @property VesselSetup[] $vesselSetups2
- * @property VesselSetup[] $vesselSetups3
- * @property VesselSetup[] $vesselSetups4
+ * @property SetupParts[] $setupParts
  */
 class Parts extends CActiveRecord
 {
@@ -51,11 +45,11 @@ class Parts extends CActiveRecord
 		// will receive user inputs.
 		return array(
 			array('serialNum, type, addedOn, addedBy', 'required'),
-			array('type, addedBy', 'numerical', 'integerOnly'=>true),
-			array('serialNum', 'length', 'max'=>30),
+			array('addedBy', 'numerical', 'integerOnly'=>true),
+			array('serialNum, type', 'length', 'max'=>30),
 			// The following rule is used by search().
 			// Please remove those attributes that should not be searched.
-			array('serialNum, name, type, description, addedOn, addedBy', 'safe', 'on'=>'search'),
+			array('serialNum, type, addedOn, addedBy', 'safe', 'on'=>'search'),
 		);
 	}
 
@@ -66,19 +60,13 @@ class Parts extends CActiveRecord
 	{
 		// NOTE: you may need to adjust the relation name and the related
 		// class name for the relations automatically generated below.
-		return array(
-			'type0' => array(self::BELONGS_TO, 'PartCategories', 'type'),
-			'addedBy0' => array(self::BELONGS_TO, 'Users', 'addedBy'),
-			'setupCamerases' => array(self::HAS_MANY, 'SetupCameras', 'camera'),
-			'setupCamerases1' => array(self::HAS_MANY, 'SetupCameras', 'lens'),
-			'setupCamerases2' => array(self::HAS_MANY, 'SetupCameras', 'filter'),
-			'vesselPlates' => array(self::HAS_MANY, 'VesselPlates', 'plate'),
-			'vesselSetups' => array(self::HAS_MANY, 'VesselSetup', 'roughPump'),
-			'vesselSetups1' => array(self::HAS_MANY, 'VesselSetup', 'turboPump'),
-			'vesselSetups2' => array(self::HAS_MANY, 'VesselSetup', 'massFlowController'),
-			'vesselSetups3' => array(self::HAS_MANY, 'VesselSetup', 'pressureGauge'),
-			'vesselSetups4' => array(self::HAS_MANY, 'VesselSetup', 'dustShaker'),
-		);
+        return array(
+            'type0' => array(self::BELONGS_TO, 'PartCategories', 'type'),
+            'addedBy0' => array(self::BELONGS_TO, 'Users', 'addedBy'),
+            'setupCamerases' => array(self::HAS_MANY, 'SetupCameras', 'lens'),
+            'setupCamerases1' => array(self::HAS_MANY, 'SetupCameras', 'filter'),
+            'setupParts' => array(self::HAS_MANY, 'SetupParts', 'part'),
+        );
 	}
 
 	/**
@@ -106,7 +94,7 @@ class Parts extends CActiveRecord
 		$criteria=new CDbCriteria;
 
 		$criteria->compare('serialNum',$this->serialNum,true);
-		$criteria->compare('type',$this->type);
+		$criteria->compare('type',$this->type,true);
 		$criteria->compare('addedOn',$this->addedOn,true);
 		$criteria->compare('addedBy',$this->addedBy);
 

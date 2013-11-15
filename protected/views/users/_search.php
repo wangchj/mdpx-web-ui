addfcfbb7a51d9934f8ffec2bfbf902e55328375
Jeff Wang <cjw39@hotmail.com>
2013-11-15 00:49:12 -0600

    Modify layout

--- protected/views/users/_search.php
+++ protected/views/users/_search.php
@@ -11,37 +11,37 @@
 	'method'=>'get',
 )); ?>
 
-	<div class="row">
+	<div>
 		<?php echo $form->label($model,'userId'); ?>
 		<?php echo $form->textField($model,'userId'); ?>
 	</div>
 
-	<div class="row">
+	<div>
 		<?php echo $form->label($model,'firstName'); ?>
 		<?php echo $form->textField($model,'firstName',array('size'=>45,'maxlength'=>45)); ?>
 	</div>
 
-	<div class="row">
+	<div>
 		<?php echo $form->label($model,'lastName'); ?>
 		<?php echo $form->textField($model,'lastName',array('size'=>45,'maxlength'=>45)); ?>
 	</div>
 
-	<div class="row">
+	<div>
 		<?php echo $form->label($model,'phone'); ?>
 		<?php echo $form->textField($model,'phone',array('size'=>10,'maxlength'=>10)); ?>
 	</div>
 
-	<div class="row">
+	<div>
 		<?php echo $form->label($model,'email'); ?>
 		<?php echo $form->textField($model,'email',array('size'=>45,'maxlength'=>45)); ?>
 	</div>
 
-	<div class="row">
+	<div>
 		<?php echo $form->label($model,'affiliation'); ?>
 		<?php echo $form->textField($model,'affiliation',array('size'=>45,'maxlength'=>45)); ?>
 	</div>
 
-	<div class="row buttons">
+	<div class="buttons">
 		<?php echo CHtml::submitButton('Search'); ?>
 	</div>
 

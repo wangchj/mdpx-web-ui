5df361559c7749b94db8e08082224012b2e5c212
Jeff Wang <cjw39@hotmail.com>
2013-11-24 13:31:26 -0600

    Enable column searching/filtering.

--- protected/views/experimentSetups/admin.php
+++ protected/views/experimentSetups/admin.php
@@ -42,12 +42,12 @@ or <b>=</b>) at the beginning of each of your search values to specify how the c
 	'dataProvider'=>$model->search(),
 	'filter'=>$model,
 	'columns'=>array(
-        'experimentSetupId::ID',
+    //    'experimentSetupId::ID',
         'experimentId::Group',
-        'dateTime:datetime:Time',
+        'dateTime:datetime',
 		'name',
 		'description',
-		'vesselSetup.name',
+		'vesselSetup.name::Vessel Setup',
 		array(
             'name'=>'dcVoltageSetpoint',
             'value'=>'round($data->dcVoltageSetpoint,2)'
